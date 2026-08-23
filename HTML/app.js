126d11b9db86b0b3c28df8b2856ef0a564d19ae0
Alexander Urvina <alexanderurvinacolin@gmail.com>
2026-08-23 01:05:56 -0600

    Se termina estructura javascript

---
+++ HTML/app.js
@@ -0,0 +1,20 @@
+//*Variables String 
+
+let nombreAlbum="A.D.R.O.M.I.C.F.M.S 4";
+console.log(nombreAlbum);
+
+let Interprete="Yung Beef";
+console.log(Interprete);
+
+//*Variables Number
+let AñoLanzamiento=2018;
+console.log(AñoLanzamiento);
+
+let Calificacion=9.5;
+console.log(Calificacion);
+
+//*Variable Boolean
+let DisponiblePlataformas=true;
+console.log(DisponiblePlataformas);
+
+
